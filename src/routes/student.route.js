fdae1315a488909dda363e1d6bb85544d8b25437
itschrizify <johnrobles11604@gmail.com>
2026-09-22 18:14:42 +0800

    Apply auth middleware to all student routes

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/student.controller");
+const authenticate = require("../middleware/auth.middleware");
 
-router.get("/", studentController.getAllStudents);
-router.get("/:id", studentController.getStudentById);
-router.post("/", studentController.createStudent);
-router.put("/:id", studentController.updateStudent);
-router.delete("/:id", studentController.deleteStudent);
+router.get("/", authenticate, studentController.getAllStudents);
+router.get("/:id", authenticate, studentController.getStudentById);
+router.post("/", authenticate, studentController.createStudent);
+router.put("/:id", authenticate, studentController.updateStudent);
+router.delete("/:id", authenticate, studentController.deleteStudent);
 
-module.exports = router;
+module.exports = router;    
